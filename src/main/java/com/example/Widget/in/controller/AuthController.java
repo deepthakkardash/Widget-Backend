f6e4804c02fbf00f24f0f204d604c5ea463615c0
Deep Thakkar <deepthakkardash24@gmail.com>
2025-10-31 19:19:27 +0530

    Minor error in response

--- src/main/java/com/example/Widget/in/controller/AuthController.java
+++ src/main/java/com/example/Widget/in/controller/AuthController.java
@@ -29,7 +29,7 @@ public class AuthController
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<user>> registerUser(@RequestBody LoginRequestDto request)
     {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(userService.RegisterUser(
+        return ResponseEntity.ok(userService.RegisterUser(
                 request.getUsername(),
                 request.getPassword(),
                 request.getFirstname(),
